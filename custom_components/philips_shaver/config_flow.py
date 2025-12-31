724db75f7c5e8ba31d4aac6e11eabf05f288c2d9
mtheli <martin.thelian@gmx.at>
2025-12-31 08:01:26 +0100

    *) Updating versions

--- custom_components/philips_shaver/config_flow.py
+++ custom_components/philips_shaver/config_flow.py
@@ -103,7 +103,7 @@ class PhilipsShaverConfigFlow(ConfigFlow, domain=DOMAIN):
     """Config flow for Philips Shaver."""
 
     VERSION = 1
-    MINOR_VERSION = 1
+    MINOR_VERSION = 2
 
     discovery_info: BluetoothServiceInfoBleak | None = None
 
